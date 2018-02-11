82b19e9af4791abe005a16c3d2e81e4ef483cb83
quarkonaut <daniel.daehler@gmail.com>
2018-02-12 00:05:49 +0100

    Migration to TypeScript
    Removed requires (does not work in chrome).

--- built/jira-innosolv-ch.user.js
+++ built/jira-innosolv-ch.user.js
@@ -1,4 +1,3 @@
-"use strict";
 // ==UserScript==
 // @name        JIRA Extensions (TS)
 // @version     1.4.5
@@ -8,15 +7,66 @@
 // @icon        https://fuse314.github.io/ico/jira-extensions.png
 // @author      Daniel Dähler, Maurus Kühne, Gottfried Mayer
 // @include     https://jira.innosolv.ch/*
-// @require     util.js
 // @grant       GM_log
 // @run-at      document-idle
 // ==/UserScript==
-Object.defineProperty(exports, "__esModule", { value: true });
-var util = require("./util");
 (function () {
     var summaryTimer;
     var commitMessageButtonTimer;
+    function shortenText(desc, maxLen) {
+        if (desc.length > maxLen) {
+            return desc.substring(0, maxLen) + "…";
+        }
+        else {
+            return desc;
+        }
+    }
+    // https://stackoverflow.com/questions/400212/how-do-i-copy-to-the-clipboard-in-javascript
+    function copyTextToClipboard(text) {
+        var textArea = document.createElement("textarea");
+        //
+        // *** This styling is an extra step which is likely not required. ***
+        //
+        // Why is it here? To ensure:
+        // 1. the element is able to have focus and selection.
+        // 2. if element was to flash render it has minimal visual impact.
+        // 3. less flakyness with selection and copying which **might** occur if
+        //    the textarea element is not visible.
+        //
+        // The likelihood is the element won't even render, not even a flash,
+        // so some of these are just precautions. However in IE the element
+        // is visible whilst the popup box asking the user for permission for
+        // the web page to copy to the clipboard.
+        //
+        // Place in top-left corner of screen regardless of scroll position.
+        textArea.style.position = 'fixed';
+        textArea.style.top = "0";
+        textArea.style.left = "0";
+        // Ensure it has a small width and height. Setting to 1px / 1em
+        // doesn't work as this gives a negative w/h on some browsers.
+        textArea.style.width = '2em';
+        textArea.style.height = '2em';
+        // We don't need padding, reducing the size if it does flash render.
+        textArea.style.padding = "0";
+        // Clean up any borders.
+        textArea.style.border = 'none';
+        textArea.style.outline = 'none';
+        textArea.style.boxShadow = 'none';
+        // Avoid flash of white box if rendered for any reason.
+        textArea.style.background = 'transparent';
+        textArea.value = text;
+        document.body.appendChild(textArea);
+        textArea.select();
+        try {
+            var successful = document.execCommand('copy');
+            var msg = successful ? 'successful' : 'unsuccessful';
+            console.log('Copying text command was ' + msg);
+        }
+        catch (err) {
+            console.log('Oops, unable to copy');
+        }
+        document.body.removeChild(textArea);
+    }
     function addCopyCommitMessageHeaderButton() {
         var source = document.getElementById("opsbar-jira.issue.tools");
         if (source == null) {
@@ -48,7 +98,7 @@ var util = require("./util");
                 taskNr = document.getElementById("key-val").innerText;
                 taskText = document.getElementById("summary-val").innerText;
             }
-            util.copyTextToClipboard(taskNr + ": " + taskText);
+            copyTextToClipboard(taskNr + ": " + taskText);
         };
         source.appendChild(newBtn);
     }
@@ -68,7 +118,7 @@ var util = require("./util");
                 if (!parentLink.dataset['issue-number']) {
                     parentLink.dataset['issue-number'] = parentLink.innerText;
                 }
-                parentLink.innerText = parentLink.dataset['issue-number'] + ": " + util.shortenText(parentLink.title, 80);
+                parentLink.innerText = parentLink.dataset['issue-number'] + ": " + shortenText(parentLink.title, 80);
             }
         }
         if (summaries.length > 0) {
--- src/jira-innosolv-ch.user.ts
+++ src/jira-innosolv-ch.user.ts
@@ -7,17 +7,79 @@
 // @icon        https://fuse314.github.io/ico/jira-extensions.png
 // @author      Daniel Dähler, Maurus Kühne, Gottfried Mayer
 // @include     https://jira.innosolv.ch/*
-// @require     util.js
 // @grant       GM_log
 // @run-at      document-idle
 // ==/UserScript==
-
-import * as util from './util'
 
 (function() {
 
     var summaryTimer;
     var commitMessageButtonTimer;
+
+    function shortenText(desc: string, maxLen: number) {
+        if(desc.length > maxLen) {
+            return desc.substring(0, maxLen) + "…";
+        } else {
+            return desc;
+        }
+    }
+    
+    // https://stackoverflow.com/questions/400212/how-do-i-copy-to-the-clipboard-in-javascript
+    function copyTextToClipboard(text: string) {
+    
+        var textArea = <HTMLTextAreaElement> document.createElement("textarea");
+    
+        //
+        // *** This styling is an extra step which is likely not required. ***
+        //
+        // Why is it here? To ensure:
+        // 1. the element is able to have focus and selection.
+        // 2. if element was to flash render it has minimal visual impact.
+        // 3. less flakyness with selection and copying which **might** occur if
+        //    the textarea element is not visible.
+        //
+        // The likelihood is the element won't even render, not even a flash,
+        // so some of these are just precautions. However in IE the element
+        // is visible whilst the popup box asking the user for permission for
+        // the web page to copy to the clipboard.
+        //
+    
+        // Place in top-left corner of screen regardless of scroll position.
+        textArea.style.position = 'fixed';
+        textArea.style.top = "0";
+        textArea.style.left = "0";
+    
+        // Ensure it has a small width and height. Setting to 1px / 1em
+        // doesn't work as this gives a negative w/h on some browsers.
+        textArea.style.width = '2em';
+        textArea.style.height = '2em';
+    
+        // We don't need padding, reducing the size if it does flash render.
+        textArea.style.padding = "0";
+    
+        // Clean up any borders.
+        textArea.style.border = 'none';
+        textArea.style.outline = 'none';
+        textArea.style.boxShadow = 'none';
+    
+        // Avoid flash of white box if rendered for any reason.
+        textArea.style.background = 'transparent';
+    
+        textArea.value = text;
+    
+        document.body.appendChild(textArea);
+    
+        textArea.select();
+    
+        try {
+            var successful = document.execCommand('copy');
+            var msg = successful ? 'successful' : 'unsuccessful';
+            console.log('Copying text command was ' + msg);
+        } catch (err) {
+            console.log('Oops, unable to copy');
+        }
+        document.body.removeChild(textArea);
+    }
 
     function addCopyCommitMessageHeaderButton() {
         var source = document.getElementById("opsbar-jira.issue.tools");
@@ -59,7 +121,7 @@ import * as util from './util'
                 taskText = document.getElementById("summary-val").innerText;
             }
 
-            util.copyTextToClipboard(taskNr + ": " + taskText);
+            copyTextToClipboard(taskNr + ": " + taskText);
         };
 
         source.appendChild(newBtn);
@@ -86,7 +148,7 @@ import * as util from './util'
                 {
                     parentLink.dataset['issue-number'] = parentLink.innerText;
                 }
-                parentLink.innerText = parentLink.dataset['issue-number'] + ": " + util.shortenText(parentLink.title, 80);
+                parentLink.innerText = parentLink.dataset['issue-number'] + ": " + shortenText(parentLink.title, 80);
             }
         }
 
